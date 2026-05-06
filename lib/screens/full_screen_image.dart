e0980cab8c4de71ab95c379e32b569e5f6dc67fb
wilbertmdp <wilbertanderson_2327240015@mhs.mdp.ac.id>
2026-05-06 20:05:20 +0700

    tambah bagian home screen
    menambah tampilan untuk setiap postingan yang telah ditambahkan, dan secara manual saya membuat tampilan nya bisa secara full screen dan bisa zoom

---
+++ lib/screens/full_screen_image.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+class FullScreenImage extends StatelessWidget {
+  final String imageBase64;
+  final String heroTag;
+
+  const FullScreenImage({
+    super.key,
+    required this.imageBase64,
+    required this.heroTag,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: Center(
+        child: Hero(
+          tag: heroTag,
+          child: InteractiveViewer(
+            minScale: 1,
+            maxScale: 5,
+            child: Image.memory(base64Decode(imageBase64), fit: BoxFit.contain),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,13 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_fasum/screens/add_post_screen.dart';
+import 'package:flutter_application_fasum/screens/full_screen_image.dart';
+import 'package:flutter_application_fasum/screens/sign_in_screen.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,11 +17,172 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Future<void> signOut(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+
+    Navigator.of(
+      context,
+    ).pushReplacement(MaterialPageRoute(builder: (context) => SignInScreen()));
+  }
+
+  String formatTime(DateTime dateTime) {
+    final now = DateTime.now();
+    final diff = now.difference(dateTime);
+    if (diff.inSeconds < 60) {
+      return '${diff.inSeconds} secs ago';
+    } else if (diff.inMinutes < 60) {
+      return '${diff.inMinutes} mins ago';
+    } else if (diff.inHours < 24) {
+      return '${diff.inHours} hrs ago';
+    } else if (diff.inHours < 48) {
+      return '1 day ago';
+    } else {
+      return DateFormat('dd/MM/yyyy').format(dateTime);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Screen")),
-      body: Center(child: Text("Welcome to the home screen")),
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
+            onPressed: () {
+              signOut(context);
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          setState(() {});
+        },
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('posts')
+              .orderBy('createdAt', descending: true)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            final posts = snapshot.data!.docs.toList();
+            return ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                final data = posts[index].data();
+                final imageBase64 = data['image'];
+                final description = data['description'];
+                final createdAtStr = data['createdAt'];
+                final fullName = data['fullName'] ?? 'Anonim';
+                final latitude = data['latitude'];
+                final longitude = data['longitude'];
+                final category = data['category'] ?? 'Lainnya';
+                final createdAt = DateTime.parse(createdAtStr);
+                String heroTag =
+                    'fasum-image-${createdAt.millisecondsSinceEpoch}';
+
+                return InkWell(
+                  onTap: () {},
+                  child: Card(
+                    elevation: 1,
+                    color: Theme.of(context).colorScheme.surfaceContainerLow,
+                    shadowColor: Theme.of(context).colorScheme.shadow,
+                    margin: const EdgeInsets.all(10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        if (imageBase64 != null)
+                          // ClipRRect(
+                          //   borderRadius: BorderRadius.circular(10),
+                          //   child: AspectRatio(
+                          //     aspectRatio: 1, // bisa 1 / 4/3 / 16/9
+                          //     child: Image.memory(
+                          //       base64Decode(imageBase64),
+                          //       fit: BoxFit.contain,
+                          //     ),
+                          //   ),
+                          // ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => FullScreenImage(
+                                    imageBase64: imageBase64,
+                                    heroTag: heroTag,
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Hero(
+                              tag: heroTag,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: AspectRatio(
+                                  aspectRatio: 1,
+                                  child: Image.memory(
+                                    base64Decode(imageBase64),
+                                    fit: BoxFit.contain,
+                                    width: double.infinity,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 8,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    formatTime(createdAt),
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  Text(
+                                    fullName,
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 6),
+                                  Text(
+                                    description ?? '',
+                                    style: const TextStyle(fontSize: 16),
+                                    maxLines: 3,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).push(
